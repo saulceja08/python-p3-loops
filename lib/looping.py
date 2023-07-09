796f46dc517f71f906fe74a42beaa200d52c8b3b
Saul Ceja <saulceja08@gmail.com>
2023-07-08 22:20:43 -0700

    Complete Lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
+    while countdown > 0:
+        print(countdown)
+        countdown -= 1
+    else:
+        print('Happy New Year!')
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    new_list = []
+    for num in int_list:
+        new_list.append(num ** 2)
+    return new_list
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
